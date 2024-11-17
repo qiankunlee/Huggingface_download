8f1a4e74e3a9da2d405c0a631f330d4ddc663803
nick lee <149494025+qiankunlee@users.noreply.github.com>
2024-11-17 13:28:48 +0800

    Update new_dataset_download.py

--- new_dataset_download.py
+++ new_dataset_download.py
@@ -2,7 +2,8 @@ import os
 from huggingface_hub import snapshot_download
  
 # 使用cache_dir参数，将模型/数据集保存到指定“本地路径”
-snapshot_download(repo_id="ibrahimhamamci/CT-RATE", repo_type="dataset",
-                  cache_dir="./down",
+snapshot_download(repo_id="jovianzm/img2vid-pexels-350k", repo_type="dataset",
+                  cache_dir="./jovianzm/img2vid-pexels-350k",
+                  local_dir="test",
                   local_dir_use_symlinks=False, resume_download=True,
-                  token='**')
+                  token='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')
