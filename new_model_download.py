388b8a75f5479ded3f647f1f3f3fb5394113b6a3
nick lee <149494025+qiankunlee@users.noreply.github.com>
2024-11-17 13:09:02 +0800

    Update new_model_download.py

--- new_model_download.py
+++ new_model_download.py
@@ -7,7 +7,7 @@ huggingface_hub.login("") # token 从 https://huggingface.co/settings/tokens 获
 
 snapshot_download(
   repo_id="meta-llama/Llama-2-70b-hf",
-  local_dir="/home/qkli/huggingface/pretrained_model",
+  local_dir="huggingface/pretrained_model",
   local_dir_use_symlinks=False,
   proxies={"https": "http://"},
   ignore_patterns=[ "*.ot", "*.msgpack", "*.onnx","*.pth"]
