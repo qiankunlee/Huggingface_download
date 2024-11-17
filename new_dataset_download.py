27a72670b561b781ce5e9043b2a569fd79223e54
nick lee <149494025+qiankunlee@users.noreply.github.com>
2024-11-17 13:32:24 +0800

    Update new_dataset_download.py

--- new_dataset_download.py
+++ new_dataset_download.py
@@ -6,4 +6,4 @@ snapshot_download(repo_id="jovianzm/img2vid-pexels-350k", repo_type="dataset",
                   cache_dir="./jovianzm/img2vid-pexels-350k",
                   local_dir="test",
                   local_dir_use_symlinks=False, resume_download=True,
-                  token='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')
+                  token='hf_**')
