c781e0e5041646981e3341506636d64959766c0f
nick lee <149494025+qiankunlee@users.noreply.github.com>
2024-11-18 09:36:02 +0800

    Update new_dataset_download.py

--- new_dataset_download.py
+++ new_dataset_download.py
@@ -1,9 +1,12 @@
 import os
 from huggingface_hub import snapshot_download
- 
+
+
+os.environ['HF_ENDPOINT']='https://hf-mirror.com'
 # 使用cache_dir参数，将模型/数据集保存到指定“本地路径”
-snapshot_download(repo_id="jovianzm/img2vid-pexels-350k", repo_type="dataset",
-                  cache_dir="./jovianzm/img2vid-pexels-350k",
+snapshot_download(repo_id="Ghaser/Wikipedia-Knowledge-2M", repo_type="dataset",
+                  cache_dir="./Ghaser/Wikipedia-Knowledge-2M",
                   local_dir="test",
                   local_dir_use_symlinks=False, resume_download=True,
-                  token='hf_**')
+                  token='hf_**',
+                  )
